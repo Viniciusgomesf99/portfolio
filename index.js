66f3f8263827a9275080af988b8915e10bab817a
Vinicius Gomes Franco <72755108+Viniciusgomesf99@users.noreply.github.com>
2023-02-12 12:54:04 -0300

    Inicial Structure HTML, css styles and JS for Categories select

---
+++ index.js
@@ -0,0 +1,66 @@
+function handleAll () {
+        const Allpages = document.querySelectorAll('.port-projects-1');
+    Allpages.forEach(element => {
+        element.setAttribute('style', 'display:inherit');
+    });
+}
+
+function handleHTMLCSS () {
+        const Allpages = document.querySelectorAll('.port-projects-1');
+    Allpages.forEach(element => {
+        element.setAttribute('style', 'display:none');
+    });
+
+    const htmlcssPages = document.querySelectorAll('.htmlcss');
+    htmlcssPages.forEach(element => {
+        element.setAttribute('style', 'display:inherit');
+    });
+}
+
+function handleJS() {
+        const Allpages = document.querySelectorAll('.port-projects-1');
+    Allpages.forEach(element => {
+        element.setAttribute('style', 'display:none');
+    });
+
+    const javascriptPages = document.querySelectorAll('.javascript');
+    javascriptPages.forEach(element => {
+        element.setAttribute('style', 'display:inherit');
+    });
+}
+
+function handleJava() {
+        const Allpages = document.querySelectorAll('.port-projects-1');
+    Allpages.forEach(element => {
+        element.setAttribute('style', 'display:none');
+    });
+
+    const javaPages = document.querySelectorAll('.java');
+    javaPages.forEach(element => {
+        element.setAttribute('style', 'display:inherit');
+    });
+}
+
+function handlePython() {
+        const Allpages = document.querySelectorAll('.port-projects-1');
+    Allpages.forEach(element => {
+        element.setAttribute('style', 'display:none');
+    });
+
+    const pythonPages = document.querySelectorAll('.python');
+    pythonPages.forEach(element => {
+        element.setAttribute('style', 'display:inherit');
+    });
+}
+
+function handleWordpress() {
+    const Allpages = document.querySelectorAll('.port-projects-1');
+    Allpages.forEach(element => {
+        element.setAttribute('style', 'display:none');
+    });
+
+    const wordpressPages = document.querySelectorAll('.wordpress');
+    wordpressPages.forEach(element => {
+        element.setAttribute('style', 'display:inherit');
+    });
+}
